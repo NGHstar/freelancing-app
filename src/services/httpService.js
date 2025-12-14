8cf0543e903e43486e549671cb00f8f8c5ba6479
NGHSTAR <nghstarcc@gmail.com>
2025-12-14 22:09:39 +0330

    fix: bug

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://freelancing-app-tau.vercel.app/api/";
+const BASE_URL = "https://shaky-sarette-ngh-be9fdd09.koyeb.app/api/";
 
 const app = axios.create({
   baseURL: BASE_URL,
